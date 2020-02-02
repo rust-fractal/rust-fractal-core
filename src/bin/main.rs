3d7ccee4e1035f839365832fc03357435e82b827
jackyarndley <34801340+jackyarndley@users.noreply.github.com>
2020-02-02 19:26:23 +1300

    Changed to use f32 rather than f64 (more than 2x speed improvement).

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -9,12 +9,12 @@ fn main() {
     let x_range = (-2.5, 1.0);
     let y_range = (-1.0, 1.0);
 
-    let x_res = (x_range.1 - x_range.0) / width as f64;
-    let y_res = (y_range.1 - y_range.0) / height as f64;
+    let x_res = (x_range.1 - x_range.0) / width as f32;
+    let y_res = (y_range.1 - y_range.0) / height as f32;
 
     let max_iterations = 1024;
 
-    let block_size = f64x8::lanes();
+    let block_size = f32x16::lanes();
     let width_in_blocks = width / block_size;
 
     let time = Instant::now();
@@ -22,20 +22,20 @@ fn main() {
         .into_par_iter()
         .map(|y_pixel| {
             let xs = unsafe {
-                let mut buf: Vec<f64x8> = vec![f64x8::splat(0.0); width_in_blocks];
+                let mut buf: Vec<f32x16> = vec![f32x16::splat(0.0); width_in_blocks];
 
-                std::slice::from_raw_parts_mut(buf.as_mut_ptr() as *mut f64, width)
+                std::slice::from_raw_parts_mut(buf.as_mut_ptr() as *mut f32, width)
                     .iter_mut()
                     .enumerate()
                     .for_each(|(j, x)| {
-                        *x = j as f64 * x_res + x_range.0
+                        *x = j as f32 * x_res + x_range.0
                     });
                 buf
             };
 
-            let y0 = f64x8::splat(y_pixel as f64 * y_res - y_range.1);
+            let y0 = f32x16::splat(y_pixel as f32 * y_res - y_range.1);
 
-            let mut out = vec![u64x8::splat(0); width_in_blocks];
+            let mut out = vec![u32x16::splat(0); width_in_blocks];
 
             out.iter_mut()
                 .enumerate()
@@ -44,22 +44,22 @@ fn main() {
                     let x0 = xs[i];
                     let mut x = x0;
                     let mut y = y0;
-                    let mut count = u64x8::splat(0);
+                    let mut count = u32x16::splat(0);
 
                     for _ in 0..max_iterations {
                         let xx = x * x;
                         let yy = y * y;
-                        let xy = f64x8::splat(2.0) * x * y;
+                        let xy = f32x16::splat(2.0) * x * y;
                         let sum = xx + yy;
                         x = xx - yy + x0;
                         y = xy + y0;
-                        let mask = sum.le(f64x8::splat(4.0));
+                        let mask = sum.le(f32x16::splat(4.0));
 
                         if mask.none() {
                             break;
                         }
 
-                        count += mask.select(u64x8::splat(1), u64x8::splat(0));
+                        count += mask.select(u32x16::splat(1), u32x16::splat(0));
                     }
                     *element = count;
                 });
@@ -67,7 +67,7 @@ fn main() {
             out
         })
         .flatten()
-        .collect::<Vec<u64x8>>();
+        .collect::<Vec<u32x16>>();
 
     println!("Rendering took {}ms", time.elapsed().as_millis());
     let time = Instant::now();
@@ -77,7 +77,7 @@ fn main() {
     let mut buffer = Vec::new();
 
     for element in pixels {
-        let mut part = vec![0u64; 8];
+        let mut part = vec![0u32; 16];
         element.write_to_slice_unaligned(&mut part);
         for element in part {
             buffer.push((element as f64 / max_iterations as f64 * 255.99) as u8)
