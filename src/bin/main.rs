2afeeadcc24b89d87133ab358a2f648d5affb22e
jackyarndley <34801340+jackyarndley@users.noreply.github.com>
2020-02-02 13:49:28 +1300

    Added SIMD using packed_simd.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,54 +1,89 @@
 use std::time::Instant;
 use rayon::prelude::*;
+use packed_simd::*;
 
 fn main() {
     let width: usize = 1920;
     let height: usize = 1080;
 
-    let x_range = (-2.5f32, 1.0f32);
+    let x_range = (-2.5, 1.0);
     let y_range = (-1.0, 1.0);
 
-    let x_res = (x_range.1 - x_range.0) / width as f32;
-    let y_res = (y_range.1 - y_range.0) / height as f32;
+    let x_res = (x_range.1 - x_range.0) / width as f64;
+    let y_res = (y_range.1 - y_range.0) / height as f64;
 
     let max_iterations = 1024;
 
+    let block_size = f64x8::lanes();
+    let width_in_blocks = width / block_size;
 
     let time = Instant::now();
     let pixels = (0..height)
         .into_par_iter()
         .map(|y_pixel| {
-            let mut iterations_buffer = Vec::with_capacity(width);
+            let xs = unsafe {
+                let mut buf: Vec<f64x8> = vec![f64x8::splat(0.0); width_in_blocks];
 
-            for x_pixel in 0..width {
-                let x0 = x_pixel as f32 * x_res + x_range.0;
-                let y0 = y_pixel as f32 * y_res - y_range.1;
-                let mut x = x0;
-                let mut y = y0;
-                let mut xx = 0.0;
-                let mut yy = 0.0;
-                let mut xy = 0.0;
-                let mut ab = 0.0;
-                let mut iterations = 0;
+                std::slice::from_raw_parts_mut(buf.as_mut_ptr() as *mut f64, width)
+                    .iter_mut()
+                    .enumerate()
+                    .for_each(|(j, x)| {
+                        *x = j as f64 * x_res + x_range.0
+                    });
+                buf
+            };
 
-                while ab < 4.0 && iterations < max_iterations {
-                    xx = x * x;
-                    yy = y * y;
-                    xy = 2.0 * x * y;
-                    iterations += 1;
-                    ab = xx + yy;
-                    x = xx - yy + x0;
-                    y = xy + y0;
-                }
-                iterations_buffer.push((iterations as f32 / max_iterations as f32 * 255.99) as u8);
-            }
+            let y0 = f64x8::splat(y_pixel as f64 * y_res - y_range.1);
 
-            iterations_buffer
+            let mut out = vec![u64x8::splat(0); width_in_blocks];
+
+            out.iter_mut()
+                .enumerate()
+                .for_each(|(i, element)| {
+                    // this is each chuck of the buffer
+                    let x0 = xs[i];
+                    let mut x = x0;
+                    let mut y = y0;
+                    let mut count = u64x8::splat(0);
+
+                    for _ in 0..max_iterations {
+                        let xx = x * x;
+                        let yy = y * y;
+                        let xy = f64x8::splat(2.0) * x * y;
+                        let sum = xx + yy;
+                        x = xx - yy + x0;
+                        y = xy + y0;
+                        let mask = sum.le(f64x8::splat(4.0));
+
+                        if mask.none() {
+                            break;
+                        }
+
+                        count += mask.select(u64x8::splat(1), u64x8::splat(0));
+                    }
+                    *element = count;
+                });
+
+            out
         })
         .flatten()
-        .collect::<Vec<u8>>();
+        .collect::<Vec<u64x8>>();
 
     println!("Rendering took {}ms", time.elapsed().as_millis());
+    let time = Instant::now();
 
-    image::save_buffer("output.png", &pixels, width as u32, height as u32, image::Gray(8)).unwrap();
+
+
+    let mut buffer = Vec::new();
+
+    for element in pixels {
+        let mut part = vec![0u64; 8];
+        element.write_to_slice_unaligned(&mut part);
+        for element in part {
+            buffer.push((element as f64 / max_iterations as f64 * 255.99) as u8)
+        }
+    }
+
+    image::save_buffer("output.png", &buffer, width as u32, height as u32, image::Gray(8)).unwrap();
+    println!("Saving took {}ms", time.elapsed().as_millis());
 }
