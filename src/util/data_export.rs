7b32fd8d46b74dbbd8bee3c069c1273573c3a839
jackyarndley <34801340+jackyarndley@users.noreply.github.com>
2020-11-30 18:21:27 +1300

    made distance estimation more colourful

--- src/util/data_export.rs
+++ src/util/data_export.rs
@@ -8,9 +8,10 @@ use std::fs::File;
 use std::slice;
 use std::collections::HashMap;
 use std::cmp::{min, max};
+use std::f32::consts::TAU;
 
 use exr::prelude::simple_image;
-use half::f16;
+use color_space::{Rgb, Hsv};
 
 pub enum DataType {
     NONE,
@@ -27,10 +28,9 @@ pub struct DataExport {
     pub rgb: Vec<u8>,
     pub palette: Vec<(u8, u8, u8)>,
     pub iterations: Vec<u32>,
-    pub smooth_f16: Vec<f16>,
-    pub smooth_f32: Vec<f32>,
-    pub distance_x: Vec<f16>,
-    pub distance_y: Vec<f16>,
+    pub smooth: Vec<f32>,
+    pub distance_x: Vec<f32>,
+    pub distance_y: Vec<f32>,
     pub display_glitches: bool,
     pub iteration_division: f32,
     pub iteration_offset: f32,
@@ -42,8 +42,7 @@ pub struct DataExport {
 impl DataExport {
     pub fn new(image_width: usize, image_height: usize, display_glitches: bool, data_type: DataType, palette: Vec<(u8, u8, u8)>, iteration_division: f32, iteration_offset: f32, analytic_derivative: bool) -> Self {
         let mut rgb = Vec::new();
-        let mut smooth_f16 = Vec::new();
-        let mut smooth_f32 = Vec::new();
+        let mut smooth = Vec::new();
         let mut distance_x = Vec::new();
         let mut distance_y = Vec::new();
 
@@ -54,23 +53,23 @@ impl DataExport {
             },
             DataType::GUI => {
                 rgb = vec![0u8; image_width * image_height * 3];
-                smooth_f32 = vec![0.0f32; image_width * image_height];
-                distance_x = vec![f16::ZERO; image_width * image_height];
-                distance_y = vec![f16::ZERO; image_width * image_height];
+                smooth = vec![0.0f32; image_width * image_height];
+                distance_x = vec![0.0f32; image_width * image_height];
+                distance_y = vec![0.0f32; image_width * image_height];
             }
             DataType::RAW => {
-                smooth_f16 = vec![f16::ZERO; image_width * image_height];
-                distance_x = vec![f16::ZERO; image_width * image_height];
-                distance_y = vec![f16::ZERO; image_width * image_height];
+                smooth = vec![0.0f32; image_width * image_height];
+                distance_x = vec![0.0f32; image_width * image_height];
+                distance_y = vec![0.0f32; image_width * image_height];
             },
             DataType::KFB => {
-                smooth_f32 = vec![0.0f32; image_width * image_height];
+                smooth = vec![0.0f32; image_width * image_height];
             },
             DataType::BOTH => {
                 rgb = vec![0u8; image_width * image_height * 3];
-                smooth_f16 = vec![f16::ZERO; image_width * image_height];
-                distance_x = vec![f16::ZERO; image_width * image_height];
-                distance_y = vec![f16::ZERO; image_width * image_height];
+                smooth = vec![0.0f32; image_width * image_height];
+                distance_x = vec![0.0f32; image_width * image_height];
+                distance_y = vec![0.0f32; image_width * image_height];
             }
         }
 
@@ -80,8 +79,7 @@ impl DataExport {
             rgb,
             palette,
             iterations: vec![0u32; image_width * image_height],
-            smooth_f16,
-            smooth_f32,
+            smooth,
             distance_x,
             distance_y,
             display_glitches,
@@ -156,7 +154,7 @@ impl DataExport {
                         let z_norm = (reference.reference_data[pixel.iteration - reference.start_iteration].z_fixed + pixel.delta_current.mantissa).norm_sqr() as f32;
                         let smooth = 1.0 - (z_norm.ln() / escape_radius_ln).log2();
 
-                        self.smooth_f32[k] = smooth;
+                        self.smooth[k] = smooth;
 
                         if self.analytic_derivative {
                             let temp1 = reference.reference_data[pixel.iteration - reference.start_iteration].z_extended + pixel.delta_current;
@@ -185,8 +183,8 @@ impl DataExport {
 
                             let output = num / ComplexFixed::new(den_1, den_2);
 
-                            self.distance_x[k] = f16::from_f64(output.re);
-                            self.distance_y[k] = f16::from_f64(output.im);
+                            self.distance_x[k] = output.re as f32;
+                            self.distance_y[k] = output.im as f32;
                         };
 
                         self.colour_index(k)
@@ -206,7 +204,7 @@ impl DataExport {
                     };
 
                     let z_norm = (reference.reference_data[pixel.iteration - reference.start_iteration].z_fixed + pixel.delta_current.mantissa).norm_sqr() as f32;
-                    self.smooth_f16[k] = f16::from_f32(1.0 - (z_norm.ln() / escape_radius_ln).log2());
+                    self.smooth[k] = 1.0 - (z_norm.ln() / escape_radius_ln).log2();
 
                     if self.analytic_derivative && pixel.iteration < self.maximum_iteration {
                         let temp1 = reference.reference_data[pixel.iteration - reference.start_iteration].z_extended + pixel.delta_current;
@@ -235,8 +233,8 @@ impl DataExport {
 
                         let output = num / ComplexFixed::new(den_1, den_2);
 
-                        self.distance_x[k] = f16::from_f64(output.re);
-                        self.distance_y[k] = f16::from_f64(output.im);
+                        self.distance_x[k] = output.re as f32;
+                        self.distance_y[k] = output.im as f32;
                     }
                 }
             },
@@ -253,7 +251,7 @@ impl DataExport {
                     };
 
                     let z_norm = (reference.reference_data[pixel.iteration - reference.start_iteration].z_fixed + pixel.delta_current.mantissa).norm_sqr() as f32;
-                    self.smooth_f32[k] = 1.0 - (z_norm.ln() / escape_radius_ln).log2();
+                    self.smooth[k] = 1.0 - (z_norm.ln() / escape_radius_ln).log2();
                 }
             },
             DataType::BOTH => {
@@ -276,7 +274,7 @@ impl DataExport {
                         let z_norm = (reference.reference_data[pixel.iteration - reference.start_iteration].z_fixed + pixel.delta_current.mantissa).norm_sqr() as f32;
                         let smooth = 1.0 - (z_norm.ln() / escape_radius_ln).log2();
 
-                        self.smooth_f16[k] = f16::from_f32(smooth);
+                        self.smooth[k] = smooth;
 
                         if self.analytic_derivative {
                             let temp1 = reference.reference_data[pixel.iteration - reference.start_iteration].z_extended + pixel.delta_current;
@@ -305,8 +303,8 @@ impl DataExport {
 
                             let output = num / ComplexFixed::new(den_1, den_2);
 
-                            self.distance_x[k] = f16::from_f64(output.re);
-                            self.distance_y[k] = f16::from_f64(output.im);
+                            self.distance_x[k] = output.re as f32;
+                            self.distance_y[k] = output.im as f32;
                         };
 
                         self.colour_index(k)
@@ -367,11 +365,11 @@ impl DataExport {
 
     pub fn save_raw(&mut self, filename: &str, approximation_order: usize, zoom: &str) {
         let iterations = simple_image::Channel::non_color_data(simple_image::Text::from("N").unwrap(), simple_image::Samples::U32(self.iterations.clone()));
-        let smooth = simple_image::Channel::non_color_data(simple_image::Text::from("NF").unwrap(), simple_image::Samples::F16(self.smooth_f16.clone()));
+        let smooth = simple_image::Channel::non_color_data(simple_image::Text::from("NF").unwrap(), simple_image::Samples::F32(self.smooth.clone()));
 
         let channels = if self.analytic_derivative {
-            let distance_x = simple_image::Channel::non_color_data(simple_image::Text::from("DEX").unwrap(), simple_image::Samples::F16(self.distance_x.clone()));
-            let distance_y = simple_image::Channel::non_color_data(simple_image::Text::from("DEY").unwrap(), simple_image::Samples::F16(self.distance_y.clone()));
+            let distance_x = simple_image::Channel::non_color_data(simple_image::Text::from("DEX").unwrap(), simple_image::Samples::F32(self.distance_x.clone()));
+            let distance_y = simple_image::Channel::non_color_data(simple_image::Text::from("DEY").unwrap(), simple_image::Samples::F32(self.distance_y.clone()));
 
             smallvec::smallvec![iterations, smooth, distance_x, distance_y]
         } else {
@@ -430,7 +428,7 @@ impl DataExport {
         }).unwrap();
 
         file.write_all(unsafe {
-            slice::from_raw_parts(self.smooth_f32.as_ptr() as *const u8, self.iterations.len() * 4)
+            slice::from_raw_parts(self.smooth.as_ptr() as *const u8, self.iterations.len() * 4)
         }).unwrap();
 
     }
@@ -444,26 +442,26 @@ impl DataExport {
             DataType::GUI => {
                 self.rgb = vec![0u8; self.image_width * self.image_height * 3];
                 self.iterations = vec![0xFFFFFFFF; self.image_width * self.image_height];
-                self.smooth_f32 = vec![0.0f32; self.image_width * self.image_height];
-                self.distance_x = vec![f16::ZERO; self.image_width * self.image_height];
-                self.distance_y = vec![f16::ZERO; self.image_width * self.image_height];
+                self.smooth = vec![0.0f32; self.image_width * self.image_height];
+                self.distance_x = vec![0.0f32; self.image_width * self.image_height];
+                self.distance_y = vec![0.0f32; self.image_width * self.image_height];
             }
             DataType::RAW => {
                 self.iterations = vec![0xFFFFFFFF; self.image_width * self.image_height];
-                self.smooth_f16 = vec![f16::ZERO; self.image_width * self.image_height];
-                self.distance_x = vec![f16::ZERO; self.image_width * self.image_height];
-                self.distance_y = vec![f16::ZERO; self.image_width * self.image_height];
+                self.smooth = vec![0.0f32; self.image_width * self.image_height];
+                self.distance_x = vec![0.0f32; self.image_width * self.image_height];
+                self.distance_y = vec![0.0f32; self.image_width * self.image_height];
             },
             DataType::KFB => {
                 self.iterations = vec![0xFFFFFFFF; self.image_width * self.image_height];
-                self.smooth_f32 = vec![0.0f32; self.image_width * self.image_height];
+                self.smooth = vec![0.0f32; self.image_width * self.image_height];
             },
             DataType::BOTH => {
                 self.rgb = vec![0u8; self.image_width * self.image_height * 3];
                 self.iterations = vec![0xFFFFFFFF; self.image_width * self.image_height];
-                self.smooth_f16 = vec![f16::ZERO; self.image_width * self.image_height];
-                self.distance_x = vec![f16::ZERO; self.image_width * self.image_height];
-                self.distance_y = vec![f16::ZERO; self.image_width * self.image_height];
+                self.smooth = vec![0.0f32; self.image_width * self.image_height];
+                self.distance_x = vec![0.0f32; self.image_width * self.image_height];
+                self.distance_y = vec![0.0f32; self.image_width * self.image_height];
             }
         }
     }
@@ -493,11 +491,11 @@ impl DataExport {
                         let k_right = pixel.image_y * self.image_width + min(self.image_width - 2, pixel.image_x) + 1;
 
                         self.iterations[k] = (self.iterations[k_up] + self.iterations[k_down] + self.iterations[k_left] + self.iterations[k_right]) / 4;
-                        self.smooth_f32[k] = (self.smooth_f32[k_up] + self.smooth_f32[k_down] + self.smooth_f32[k_left] + self.smooth_f32[k_right]) / 4.0;
+                        self.smooth[k] = (self.smooth[k_up] + self.smooth[k_down] + self.smooth[k_left] + self.smooth[k_right]) / 4.0;
 
                         if self.analytic_derivative {
-                            self.distance_x[k] = f16::from_f32((f32::from(self.distance_x[k_up]) + f32::from(self.distance_x[k_down]) + f32::from(self.distance_x[k_left]) + f32::from(self.distance_x[k_right])) / 4.0);
-                            self.distance_y[k] = f16::from_f32((f32::from(self.distance_y[k_up]) + f32::from(self.distance_y[k_down]) + f32::from(self.distance_y[k_left]) + f32::from(self.distance_y[k_right])) / 4.0);
+                            self.distance_x[k] = (self.distance_x[k_up] + self.distance_x[k_down] + self.distance_x[k_left] + self.distance_x[k_right]) / 4.0;
+                            self.distance_y[k] = (self.distance_y[k_up] + self.distance_y[k_down] + self.distance_y[k_left] + self.distance_y[k_right]) / 4.0;
                         }
 
                         self.colour_index(k);
@@ -514,15 +512,32 @@ impl DataExport {
             self.rgb[3 * i + 1] = 0u8; 
             self.rgb[3 * i + 2] = 0u8;
         } else if self.analytic_derivative {
-            let output = ((f32::from(self.distance_x[i])).powi(2) + (f32::from(self.distance_y[i])).powi(2)).sqrt();
+            let length = (self.distance_x[i].powi(2) + self.distance_y[i].powi(2)).sqrt();
+            
+            // colouring algorithm based on 'rainbow_fringe' by claude
+            let angle = self.distance_y[i].atan2(self.distance_x[i]);
 
-            let out = (255.0 * output.tanh()) as u8;
+            let mut hue = angle / TAU;
+            hue -= hue.floor();
 
-            self.rgb[3 * i] = out as u8; 
-            self.rgb[3 * i + 1] = out as u8; 
-            self.rgb[3 * i + 2] = out as u8;
+            let saturation = (1.0 / (1.0 + length)).max(0.0).min(1.0);
+            let value = length.max(0.0).min(1.0);
+
+            let hsv = Hsv::new(hue as f64 * 360.0, saturation as f64, value as f64);
+            let rgb = Rgb::from(hsv);
+
+            self.rgb[3 * i] = rgb.r as u8; 
+            self.rgb[3 * i + 1] = rgb.g as u8; 
+            self.rgb[3 * i + 2] = rgb.b as u8;
+
+            // default colouring algorithm
+            // let out = (255.0 * length) as u8;
+
+            // self.rgb[3 * i] = out as u8; 
+            // self.rgb[3 * i + 1] = out as u8; 
+            // self.rgb[3 * i + 2] = out as u8;
         } else {
-            let temp = ((self.iterations[i] as f32 + self.smooth_f32[i]) / self.iteration_division) + self.iteration_offset;
+            let temp = ((self.iterations[i] as f32 + self.smooth[i]) / self.iteration_division) + self.iteration_offset;
 
             let temp2 = temp.floor() as usize % self.palette.len();
             let temp3 = (temp as usize + 1) % self.palette.len();
